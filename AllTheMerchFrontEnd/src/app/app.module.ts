1cee2ada84b831391602ec656b29461a3293fea6
Sergiogamboa11 <sergio.gamboa11@gmail.com>
2020-07-14 14:59:22 -0600

    Added register component with no functionality

--- AllTheMerchFrontEnd/src/app/app.module.ts
+++ AllTheMerchFrontEnd/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
