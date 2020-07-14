82cd7ef46288f958bc1fd7831b57bda304fa845e
Sergiogamboa11 <sergio.gamboa11@gmail.com>
2020-07-14 15:44:56 -0600

    Added new shop component, made default route take user to shop, made logo route to shop

--- AllTheMerchFrontEnd/src/app/app-routing.module.ts
+++ AllTheMerchFrontEnd/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { ShopComponent } from './shop/shop.component';
 
 
 const routes: Routes = [
+  {path: 'shop', component: ShopComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: '', component: ShopComponent}, 
+  {path: '**', component: ShopComponent}
 ];
 
 @NgModule({
--- AllTheMerchFrontEnd/src/app/app.module.ts
+++ AllTheMerchFrontEnd/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RegisterComponent } from './register/register.component';
+import { ShopComponent } from './shop/shop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ShopComponent
   ],
   imports: [
     BrowserModule,
