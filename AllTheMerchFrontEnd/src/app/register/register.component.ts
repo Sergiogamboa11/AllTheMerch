c555c9c6b7c22bb41c304242c2a886a2af3ae33e
Sergiogamboa11 <sergio.gamboa11@gmail.com>
2020-07-16 20:22:52 -0600

    Created UI form in register component with no functionality

--- AllTheMerchFrontEnd/src/app/register/register.component.ts
+++ AllTheMerchFrontEnd/src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router, ɵangular_packages_router_router_e } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,7 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm = new FormGroup({
+    usernameForm: new FormControl(''),
+    passwordForm: new FormControl(''),
+    passwordConfirmForm: new FormControl(''),
+  });
+
+  createAccount(){
+    this.router.navigateByUrl('/login');
+  }
+
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
