ec1f78dd1a198f191169c56809ea88190f40cd6c
Sergiogamboa11 <sergio.gamboa11@gmail.com>
2020-08-04 16:40:50 -0600

    Made UI changes to item listing component

--- AllTheMerchFrontEnd/src/app/item-listing/item-listing.component.ts
+++ AllTheMerchFrontEnd/src/app/item-listing/item-listing.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+import { CartItem } from '../models/cart-item.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-item-listing',
@@ -11,12 +13,30 @@ export class ItemListingComponent implements OnInit {
 
   public itemId;
   public item;
+  private cartItem: CartItem = new CartItem();
+
+  registerForm = new FormGroup({
+    quantityForm: new FormControl('')
+  });
 
   constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit(){
     this.itemId = this.router.url.substring(this.router.url.lastIndexOf('/') + 1);
     this.getShopItem();
+  }
+
+  addToCart(){
+
+    this.cartItem.cartId = 1; //This is hardcoded for now
+    this.cartItem.quantity = this.registerForm.get("quantityForm").value;
+    this.cartItem.itemId = this.itemId;
+
+    this.http.post('http://localhost:9025/api/cart/', this.cartItem).toPromise().then(data => {
+      if(data == null || data == undefined){
+      } else{
+      }
+    });
   }
 
   getShopItem(){
